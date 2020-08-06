5110d25ca7e95f2e7b5932e8c906fa9958a94e29
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-06 21:08:04 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ test
 module.exports = { name, height
 }
 
-if (Expected: Susan, Recieved: Joe)
+if (Expected: Susan, Recieved:Joe)
 console.log("the name is correct")
 if (Expected: 70, Recieved: 74)
 console.log("the height is correct")
